517bee561d8ec3a02c2cadbd714da7e4605554dd
jutao622 <113498226+jutao622@users.noreply.github.com>
2022-11-12 16:37:21 +0800

    小程序学习

---
+++ weapp/didi/pages/index/index.js
@@ -0,0 +1,46 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    title:'我的Dream car迈凯伦',
+    car:'https://p3.dcarimg.com/img/mosaic-legacy/bef00000e39673bd00df~332x0.webp'
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    setTimeout(()=>{
+      this.setData({
+        title:'我的Dream car迈凯伦',
+        car:'https://p3.dcarimg.com/img/mosaic-legacy/bef00000e39673bd00df~332x0.webp'
+      })
+    })     
+  },
+ 
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
---
+++ weapp/discavry/readme.md
@@ -0,0 +1,34 @@
+#小程序 云开发
+
+##现代开发方式
+1.小程序 界面直接打开 4s店做一款试驾应用
+2.
+3.
+4.
+ 
+
+
+##小程序开发知识点
+1.开发一个个page
+    /page
+2.page结构
+    wxml    结构
+        动态模板
+    wxss    样式
+        vw  适配所有手机
+        vh  
+        绝对居中 
+        page    每个页面隐藏的  标签选择    1
+        .container{} 类选择器   10
+        page 相对定位 relative 100vw 100vh
+        contaienr absolute
+            t 50%   l 50%   中心点
+            transform:translate(-50%,-50%)
+    js  逻辑
+        page({
+            data:{//可绑定的数据
+            
+            }
+
+
+            })
